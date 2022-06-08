9716445f15fad5b39aebad3b83b982b94c2f2cc9
Hamza Anwar <88555920+hamzaanwardev@users.noreply.github.com>
2022-06-09 01:29:56 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
 let js = 'hamza';
-if (js === 'hamza') { alert ("True");}
+// if (js === 'hamza') { alert ("True");}
 
 // 10 + 30 + 71 - 23;
 console.log(10+30+71-23);
+
+// Declaring a variable
+let firstName = 'hamza';
+console.log(firstName);
+
+// Conventions for variable name
+// let new = "saad";   //we can not use word new as variable name
+// let function = 'jamal'; //we can not use word function as variable name
+// let 3years = '2015' + '2016' + '2017';  //we can not use number as first letter in variable name.
+
+// We can use $, _, camelcase, number after any alphabet as variable name. 
+
+
+
+// Always use relevent variable names with respect to its value. so everyone can understand easily
+// Here we can understand my FirstJob and myCurrentJob.
+let myFirstJob = 'Mercahnt Navy Oficer';
+let myCurrentJob = 'Programmer';
+
+// Not Relevant because we don't know what is job1 and job2.
+let job1 = "Mercant Navy Officer";
+let job2 = "Programmer";
